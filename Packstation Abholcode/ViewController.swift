c26f2512ab9a5cd7a8ed5f4575041e97a7210b01
Dave Nicolson <david.nicolson@gmail.com>
2021-05-24 22:31:31 +0200

    Show Abholcode on iOS

--- Packstation Abholcode/ViewController.swift
+++ Packstation Abholcode/ViewController.swift
@@ -8,6 +8,25 @@
 import UIKit
 import GoogleSignIn
 import GTMAppAuth
+import GoogleAPIClientForREST
+
+extension String {
+    func urlSafeBase64Decoded() -> String? {
+        var st = self
+            .replacingOccurrences(of: "_", with: "/")
+            .replacingOccurrences(of: "-", with: "+")
+        let remainder = self.count % 4
+        if remainder > 0 {
+            st = self.padding(toLength: self.count + 4 - remainder,
+                              withPad: "=",
+                              startingAt: 0)
+        }
+        guard let d = Data(base64Encoded: st, options: .ignoreUnknownCharacters) else{
+            return nil
+        }
+        return String(data: d, encoding: .utf8)
+    }
+}
 
 class ViewController: UIViewController {
 
@@ -25,6 +44,7 @@ class ViewController: UIViewController {
 
     @objc func userDidSignInGoogle(_ notification: Notification) {
         updateScreen()
+        updateAbholcode()
     }
 
     override func viewDidLoad() {
@@ -132,6 +152,63 @@ class ViewController: UIViewController {
             introLabel.isHidden = false
             signInButton.isHidden = false
             signOutButton.isHidden = true
+            abholcodeView.isHidden = true
+        }
+    }
+
+    func updateAbholcode() {
+        guard GIDSignIn.sharedInstance()?.currentUser != nil else {
+            return
+        }
+
+        let abholcodeLabel = abholcodeView.subviews[1] as? UILabel
+        let authorizer = GTMAppAuthFetcherAuthorization(fromKeychainForName: "Gmail")
+        queryAbholcode(authorizer: authorizer!) {(code: String) in
+            abholcodeLabel!.text = code
+            self.abholcodeView.isHidden = false
+        }
+    }
+
+    func queryAbholcode(authorizer: GTMFetcherAuthorizationProtocol, completion : @escaping (String) -> Void) {
+        let gmailService = GTLRGmailService.init()
+        //let authorizer = GIDSignIn.sharedInstance()?.currentUser?.authentication?.fetcherAuthorizer()
+        gmailService.authorizer = authorizer
+
+        let listQuery = GTLRGmailQuery_UsersMessagesList.query(withUserId: "me")
+        listQuery.q = "from:noreply.packstation@dhl.de subject:Abholcode"
+        listQuery.labelIds = ["INBOX"]
+
+        gmailService.executeQuery(listQuery) { (ticket, response, error) in
+            if response != nil {
+                let response = response as! GTLRGmail_ListMessagesResponse
+                let identifier = response.messages![0].identifier
+                let messageQuery = GTLRGmailQuery_UsersMessagesGet.query(withUserId: "me", identifier: identifier ?? "")
+                messageQuery.identifier = identifier
+
+                gmailService.executeQuery(messageQuery) { (ticket, response, error) in
+                    if response != nil {
+                        let message = response as! GTLRGmail_Message
+                        let base64encodedData = message.payload?.parts?[0].parts?[0].body?.data!
+                        let str = base64encodedData!.urlSafeBase64Decoded()!
+
+                        let pattern = "\n([0-9]{4})"
+                        let regex = try! NSRegularExpression(pattern: pattern)
+                        let result = regex.firstMatch(in:str, range:NSMakeRange(0, str.utf16.count))
+                        let range = result!.range(at:1)
+                        if let swiftRange = Range(range, in: str) {
+                            let name = str[swiftRange]
+                            print(name)
+                            completion(String(name) as String)
+                        }
+                    } else {
+                        print("Error: ")
+                        print(error!)
+                    }
+                }
+            } else {
+                print("Error: ")
+                print(error!)
+            }
         }
     }
 }
