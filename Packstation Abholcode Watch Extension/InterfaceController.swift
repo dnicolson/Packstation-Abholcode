df39c4833d5d655858dd978abca48c98e4ba1a0a
Dave Nicolson <david.nicolson@gmail.com>
2021-06-04 23:56:19 +0200

    Improve Gmail query structure

--- Packstation Abholcode Watch Extension/InterfaceController.swift
+++ Packstation Abholcode Watch Extension/InterfaceController.swift
@@ -9,25 +9,6 @@ import WatchKit
 import WatchConnectivity
 import Foundation
 import GTMAppAuth
-import GoogleAPIClientForREST
-
-extension String {
-    func urlSafeBase64Decoded() -> String? {
-        var st = self
-            .replacingOccurrences(of: "_", with: "/")
-            .replacingOccurrences(of: "-", with: "+")
-        let remainder = self.count % 4
-        if remainder > 0 {
-            st = self.padding(toLength: self.count + 4 - remainder,
-                              withPad: "=",
-                              startingAt: 0)
-        }
-        guard let d = Data(base64Encoded: st, options: .ignoreUnknownCharacters) else{
-            return nil
-        }
-        return String(data: d, encoding: .utf8)
-    }
-}
 
 class InterfaceController: WKInterfaceController, WCSessionDelegate {
 
@@ -37,49 +18,6 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
     @IBOutlet weak var introText: WKInterfaceLabel!
     @IBOutlet weak var abholcodeGroup: WKInterfaceGroup!
     @IBOutlet weak var abholcode: WKInterfaceLabel!
-
-    func queryAbholcode(authorizer: GTMFetcherAuthorizationProtocol, completion : @escaping (String) -> Void) {
-        let gmailService = GTLRGmailService.init()
-        //let authorizer = GIDSignIn.sharedInstance()?.currentUser?.authentication?.fetcherAuthorizer()
-        gmailService.authorizer = authorizer
-
-        let listQuery = GTLRGmailQuery_UsersMessagesList.query(withUserId: "me")
-        listQuery.q = "from:noreply.packstation@dhl.de subject:Abholcode"
-        listQuery.labelIds = ["INBOX"]
-
-        gmailService.executeQuery(listQuery) { (ticket, response, error) in
-            if response != nil {
-                let response = response as! GTLRGmail_ListMessagesResponse
-                let identifier = response.messages![0].identifier
-                let messageQuery = GTLRGmailQuery_UsersMessagesGet.query(withUserId: "me", identifier: identifier ?? "")
-                messageQuery.identifier = identifier
-
-                gmailService.executeQuery(messageQuery) { (ticket, response, error) in
-                    if response != nil {
-                        let message = response as! GTLRGmail_Message
-                        let base64encodedData = message.payload?.parts?[0].parts?[0].body?.data!
-                        let str = base64encodedData!.urlSafeBase64Decoded()!
-
-                        let pattern = "\n([0-9]{4})"
-                        let regex = try! NSRegularExpression(pattern: pattern)
-                        let result = regex.firstMatch(in:str, range:NSMakeRange(0, str.utf16.count))
-                        let range = result!.range(at:1)
-                        if let swiftRange = Range(range, in: str) {
-                            let name = str[swiftRange]
-                            print(name)
-                            completion(String(name) as String)
-                        }
-                    } else {
-                        print("Error: ")
-                        print(error!)
-                    }
-                }
-            } else {
-                print("Error: ")
-                print(error!)
-            }
-        }
-    }
 
     let session = WCSession.default
 
@@ -92,7 +30,7 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
         let authorizer = GTMAppAuthFetcherAuthorization(fromKeychainForName: "Gmail")
 
         if (authorizer != nil) {
-            queryAbholcode(authorizer: authorizer!) {(code: String) in
+            queryAbholcodeGmail(authorizer: authorizer!) {(code: String) in
                 self.abholcode.setText(code)
                 self.introText.setHidden(true)
                 self.abholcodeGroup.setHidden(false)
---
+++ Packstation Abholcode/Helpers/AbholcodeGmail.swift
@@ -0,0 +1,69 @@
+//
+//  AbholcodeGmail.swift
+//  Packstation Abholcode
+//
+//  Created by Dave Nicolson on 04.06.21.
+//
+
+import Foundation
+import GTMAppAuth
+import GoogleAPIClientForREST
+
+extension String {
+    func urlSafeBase64Decoded() -> String? {
+        var st = self
+            .replacingOccurrences(of: "_", with: "/")
+            .replacingOccurrences(of: "-", with: "+")
+        let remainder = self.count % 4
+        if remainder > 0 {
+            st = self.padding(toLength: self.count + 4 - remainder,
+                              withPad: "=",
+                              startingAt: 0)
+        }
+        guard let d = Data(base64Encoded: st, options: .ignoreUnknownCharacters) else {
+            return nil
+        }
+        return String(data: d, encoding: .utf8)
+    }
+}
+
+func queryAbholcodeGmail(authorizer: GTMFetcherAuthorizationProtocol, completion : @escaping (String) -> Void) {
+    let gmailService = GTLRGmailService.init()
+    //let authorizer = GIDSignIn.sharedInstance()?.currentUser?.authentication?.fetcherAuthorizer()
+    gmailService.authorizer = authorizer
+
+    let listQuery = GTLRGmailQuery_UsersMessagesList.query(withUserId: "me")
+    listQuery.q = "from:noreply.packstation@dhl.de subject:Abholcode"
+    listQuery.labelIds = ["INBOX"]
+
+    gmailService.executeQuery(listQuery) { (ticket, response, error) in
+        if response != nil {
+            let response = response as! GTLRGmail_ListMessagesResponse
+            let identifier = response.messages![0].identifier
+            let messageQuery = GTLRGmailQuery_UsersMessagesGet.query(withUserId: "me", identifier: identifier ?? "")
+            messageQuery.identifier = identifier
+
+            gmailService.executeQuery(messageQuery) { (ticket, response, error) in
+                if response != nil {
+                    let message = response as! GTLRGmail_Message
+                    let base64encodedData = message.payload?.parts?[0].parts?[0].body?.data!
+                    let str = base64encodedData!.urlSafeBase64Decoded()!
+
+                    let pattern = "\n([0-9]{4})"
+                    let regex = try! NSRegularExpression(pattern: pattern)
+                    let result = regex.firstMatch(in:str, range:NSMakeRange(0, str.utf16.count))
+                    let range = result!.range(at:1)
+                    if let swiftRange = Range(range, in: str) {
+                        let name = str[swiftRange]
+                        print("Abholcode:", name)
+                        completion(String(name) as String)
+                    }
+                } else {
+                    print("Abholcode error:", error!)
+                }
+            }
+        } else {
+            print("Abholcode error:", error!)
+        }
+    }
+}
