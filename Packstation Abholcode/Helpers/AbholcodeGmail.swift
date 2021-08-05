0f818b26f8a2fd3be86c5d2a0be985ea3e1ce131
Dave Nicolson <david.nicolson@gmail.com>
2021-08-05 17:34:15 +0200

    Check snippet instead of full email

--- Packstation Abholcode/Helpers/AbholcodeGmail.swift
+++ Packstation Abholcode/Helpers/AbholcodeGmail.swift
@@ -9,24 +9,6 @@ import Foundation
 import GTMAppAuth
 import GoogleAPIClientForREST
 
-extension String {
-    func urlSafeBase64Decoded() -> String? {
-        var st = self
-            .replacingOccurrences(of: "_", with: "/")
-            .replacingOccurrences(of: "-", with: "+")
-        let remainder = self.count % 4
-        if remainder > 0 {
-            st = self.padding(toLength: self.count + 4 - remainder,
-                              withPad: "=",
-                              startingAt: 0)
-        }
-        guard let d = Data(base64Encoded: st, options: .ignoreUnknownCharacters) else {
-            return nil
-        }
-        return String(data: d, encoding: .utf8)
-    }
-}
-
 func queryAbholcodeGmail(authorizer: GTMFetcherAuthorizationProtocol, completion : @escaping (String) -> Void) {
     let gmailService = GTLRGmailService.init()
     // let authorizer = GIDSignIn.sharedInstance()?.currentUser?.authentication?.fetcherAuthorizer()
@@ -46,12 +28,16 @@ func queryAbholcodeGmail(authorizer: GTMFetcherAuthorizationProtocol, completion
             gmailService.executeQuery(messageQuery) { (_, response, error) in
                 if response != nil {
                     let message = response as! GTLRGmail_Message
-                    let base64encodedData = message.payload?.parts?[0].parts?[0].body?.data!
-                    let str = base64encodedData!.urlSafeBase64Decoded()!
+                    let str = message.snippet!
 
-                    let pattern = "\n([0-9]{4})"
+                    let pattern = "lautet: ([0-9]{4})"
                     let regex = try! NSRegularExpression(pattern: pattern)
-                    let result = regex.firstMatch(in: str, range: NSRange(location: 0, length: str.utf16.count))
+                    let result = regex.firstMatch(in: str, range: NSRange(location: 0, length: str.count))
+                    if result == nil {
+                        print("Abholcode not found in email")
+                        return
+                    }
+
                     let range = result!.range(at: 1)
                     if let swiftRange = Range(range, in: str) {
                         let name = str[swiftRange]
